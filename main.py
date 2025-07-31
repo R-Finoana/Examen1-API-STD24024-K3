3ba38880bf11084d52088739aa4c3c5867482f7f
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2025-07-31 08:47:04 +0300

    feat: add Q2 answers

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ app = FastAPI()
 def get_ping(request: Request):
     return Response(content="pong", status_code=200, media_type="text/plain")
 
+@app.get("/home")
+def welcome_user(request: Request):
+    with open("welcome.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+        return Response(content=html_content, status_code=200, media_type="text/html")
 
 @app.post("/")
 def create_post():
